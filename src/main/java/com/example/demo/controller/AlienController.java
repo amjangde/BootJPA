315823196bf66cd6a9f3e0d7429ceb89ebe02d88
Aman <amanjangde02@gmail.com>
2024-03-01 23:12:26 +0530

    POST with Raw+JSON for REST impl

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -56,7 +57,7 @@ public class AlienController {
 	//REST API
 	
 	@PostMapping("/alien")
-	public Alien addAlien(Alien alien)
+	public Alien addAlien(@RequestBody Alien alien)
 	{
 		repo.save(alien);
 		return alien;
