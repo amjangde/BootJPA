76f774ea543f8bed8eee4233959eaea8ca7e48ea
Aman <amanjangde02@gmail.com>
2024-03-01 23:32:18 +0530

    Put for REST impl

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,6 +71,13 @@ public class AlienController {
 		Alien alien = repo.getOne(aId);
 		repo.delete(alien);
 		return "Deleted";
+	}
+	
+	@PutMapping(path="/alien")
+	public Alien putAlien(@RequestBody Alien alien)
+	{
+		repo.save(alien);
+		return alien;
 	}
 	
 //	@RequestMapping(path="/aliens", produces= {"application/xml"})
